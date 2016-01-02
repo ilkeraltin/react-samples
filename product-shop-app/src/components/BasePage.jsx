45ea7fbedf9febe5c4871844729ac00f88a70d40
ilker ALTIN <ilkerobi@gmail.com>
2016-01-02 19:09:28 +0200

    lead capture form added to project

--- product-shop-app/src/components/BasePage.jsx
+++ product-shop-app/src/components/BasePage.jsx
@@ -1,29 +1,38 @@
-var React=require('react');
-var NavBar=require('./nav/NavBar.jsx');
-var navLinks=[
+var React = require('react');
+var NavBar = require('./nav/NavBar.jsx');
+var LeadCapture = require('./forms/LeadCapture.jsx');
+var navLinks = [
   {
-    title:"Home",
-    href:"/"
-  },
-  {
-    title:"React Course",
-    href:"/product/345"
-  },
-  {
-    title:"Flux Course",
-    href:"/product/456456"
+    title: "Home",
+    href: "/"
+  }, {
+    title: "React Course",
+    href: "/product/345"
+  }, {
+    title: "Flux Course",
+    href: "/product/456456"
   }
 ];
-var BasePage= React.createClass({
+var BasePage = React.createClass({
   render: function() {
     return (
       <div>
-      <NavBar bgColor="#fff" titleColor="#3097d1"  navData={navLinks}/>
-      {this.props.children}
-    </div>
+        <NavBar bgColor="#fff" titleColor="#3097d1" navData={navLinks}/>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-9">
+              {this.props.children}
+            </div>
+            <div className="col-sm-3">
+              <LeadCapture/>
+            </div>
+          </div>
+
+        </div>
+      </div>
     );
   }
 
 });
 
-module.exports=BasePage;
+module.exports = BasePage;
