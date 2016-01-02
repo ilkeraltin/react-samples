3abc73f3169b9fa95b57b24f758373160f888853
ilker ALTIN <ilkerobi@gmail.com>
2016-01-02 13:35:20 +0200

    added react skeleton and simple express server

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-samples
+React Skeletons & Sample Projects
---
+++ simple-express-server-master/server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+//Allow all requests from all domains & localhost
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET");
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+var ingredients = [
+  {"id":1,"text":"ham"},
+  {"id":2,"text":"cheese"},
+  {"id":3,"text":"potatoes"},
+  {"id":4,"text":"beer"}
+];
+
+
+
+app.get('/ingredients',function(req,res){
+  res.send(ingredients);
+});
+
+app.post('/ingredients', function(req, res) {
+    var ingredient = req.body;
+    console.log(req.body);
+    ingredients.push(ingredient);
+    res.status(200).send("Successfully posted ingredient");
+});
+
+app.listen(6069);
